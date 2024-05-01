bd3d16a8a9fab68b7d72dd33c3a4bcd3935d0a45
RuslanGon <ruslan441006@gmail.com>
2024-05-01 19:14:44 +0300

    wer

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -13,23 +13,24 @@ const ProductDetailsPage = () => {
     async function fetchProductDetails() {
       try {
         const data = await reguestProductDetailsById(productId);
-        setProductDetails(data)
+        setProductDetails(data);
       } catch (error) {
         console.log(error);
       }
     }
-    fetchProductDetails()
+    fetchProductDetails();
   }, [productId]);
 
   return (
     <div>
       <h1>Product details {productId}</h1>
+      {productDetails !== null && 
       <div>
         <img src={productDetails.thumbnail} alt={productDetails.title} />
         <h3>Title: {productDetails.title} </h3>
         <p>Brand: {productDetails.brand}</p>
         <p>Price: {productDetails.price}</p>
-      </div>
+      </div>}
     </div>
   )
 }
