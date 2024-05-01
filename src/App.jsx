65dc84c52668bef34074e29ac962dd70c873afa5
RuslanGon <ruslan441006@gmail.com>
2024-05-01 16:36:06 +0300

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-
 import { NavLink } from "react-router-dom";
 import "./App.css";
+import css from './App.module.css'
 
 function App() {
-  
   return (
-    
     <div>
-     <header>
-      {/* <a target="blank" rel='noopener noreferrer' href="/products">Products</a> */}
-    <NavLink to='/mailbox'>MailBos</NavLink>
-    <NavLink to='/products'>Products</NavLink>
-    <NavLink to='/search'>Search</NavLink>
-
-     </header>
+      <header>
+        <nav className={css.nav}>
+          {/* <a target="blank" rel='noopener noreferrer' href="/products">Products</a> */}
+          <NavLink className={css.navlink} to="/mailbox">MailBos</NavLink>
+          <NavLink className={css.navlink} to="/products">Products</NavLink>
+          <NavLink className={css.navlink} to="/search">Search</NavLink>
+        </nav>
+      </header>
     </div>
   );
 }
