84b3a771043c3fb53ba893ddbf1f48c19cee0b66
RuslanGon <ruslan441006@gmail.com>
2024-05-01 16:47:58 +0300

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { NavLink } from "react-router-dom";
 import "./App.css";
 import css from './App.module.css'
+import clsx from "clsx";
 
 function App() {
+
+const getNavLinkClass = ({isActive}) => clsx(css.navlink, {
+  [css.active] : isActive
+}) 
+
   return (
     <div>
       <header>
         <nav className={css.nav}>
           {/* <a target="blank" rel='noopener noreferrer' href="/products">Products</a> */}
-          <NavLink className={css.navlink} to="/mailbox">MailBos</NavLink>
-          <NavLink className={css.navlink} to="/products">Products</NavLink>
-          <NavLink className={css.navlink} to="/search">Search</NavLink>
+          <NavLink className={getNavLinkClass} to="/mailbox">MailBos</NavLink>
+          <NavLink className={getNavLinkClass} to="/products">Products</NavLink>
+          <NavLink className={getNavLinkClass} to="/search">Search</NavLink>
         </nav>
       </header>
     </div>
