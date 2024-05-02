5b95db543f23ebba4bbef5e49071920d1c3c099e
RuslanGon <ruslan441006@gmail.com>
2024-05-02 15:13:08 +0300

    wer

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,8 +1,13 @@
-import { useEffect, useRef, useState } from "react"
+import { Suspense, lazy, useEffect, useRef, useState } from "react"
 import { Link, Route, Routes, useLocation, useParams } from "react-router-dom"
 import { reguestProductDetailsById } from "../serveses/api"
-import CommentPage from "./CommentPage"
-import ReviesPage from "./ReviesPage"
+import Loader from "../component/Loader/Loader"
+
+// import CommentPage from "./CommentPage"
+// import ReviesPage from "./ReviesPage"
+
+const CommentPage = lazy(() => import('./CommentPage'))
+const ReviesPage = lazy(() => import('./ReviesPage'))
 
 
 const ProductDetailsPage = () => {
@@ -30,21 +35,24 @@ const ProductDetailsPage = () => {
     <div>
       <h1>Product details {productId}</h1>
       <Link to={backLinkRef.current}>←Go back</Link>
-      {productDetails !== null && 
-      <div>
-        <img src={productDetails.thumbnail} alt={productDetails.title} />
-        <h3>Title: {productDetails.title} </h3>
-        <p>Brand: {productDetails.brand}</p>
-        <p>Price: {productDetails.price}</p>
-      </div>}
-      <Link to='comments'>Comments</Link>
-      <Link to='reviews'>Revies</Link>
-      <Routes>
-      <Route path="comments" element={<CommentPage />} />
-      <Route path="reviews" element={<ReviesPage />} />
-      </Routes>
+      {productDetails !== null && (
+        <div>
+          <img src={productDetails.thumbnail} alt={productDetails.title} />
+          <h3>Title: {productDetails.title} </h3>
+          <p>Brand: {productDetails.brand}</p>
+          <p>Price: {productDetails.price}</p>
+        </div>
+      )}
+      <Link to="comments">Comments</Link>
+      <Link to="reviews">Revies</Link>
+      <Suspense fallback={<Loader />}> 
+        <Routes>
+          <Route path="comments" element={<CommentPage />} />
+          <Route path="reviews" element={<ReviesPage />} />
+        </Routes>
+      </Suspense>
     </div>
-  )
+  );
 }
 
 export default ProductDetailsPage
