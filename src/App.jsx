83250b40ada952ebcc354cc684d82b32bb79f150
RuslanGon <ruslan441006@gmail.com>
2024-05-02 14:55:40 +0300

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import { NavLink, Route, Routes } from "react-router-dom";
 // import "./App.css";
 import css from './App.module.css'
 import clsx from "clsx";
-import MailBoxPage from './pages/MailBoxPage'
-import ProductsPage from './pages/ProductsPage'
-import SearchPage from './pages/SearchPage'
-import HomePage from "./pages/HomePage";
-import NotFoundPage from "./pages/NotFoundPage";
-import ProductDetailsPage from "./pages/ProductDetailsPage";
+import { Suspense, lazy } from "react";
+
+
+// import MailBoxPage from './pages/MailBoxPage'
+// import ProductsPage from './pages/ProductsPage'
+// import SearchPage from './pages/SearchPage'
+// import HomePage from "./pages/HomePage";
+// import NotFoundPage from "./pages/NotFoundPage";
+// import ProductDetailsPage from "./pages/ProductDetailsPage";
+
+const MailBoxPage = lazy(() => import('./pages/MailBoxPage'))
+const ProductsPage = lazy(() => import('./pages/ProductsPage'))
+const SearchPage = lazy(() => import('./pages/SearchPage'))
+const HomePage = lazy(() => import('./pages/HomePage'))
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage'))
+const ProductDetailsPage = lazy(() => import('./pages/ProductDetailsPage'))
+
 
 function App() {
 
@@ -35,14 +46,16 @@ const getNavLinkClass = ({isActive}) => clsx(css.navlink, {
         </nav>
       </header>
       <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/mailbox" element={<MailBoxPage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/products/:productId/*" element={<ProductDetailsPage />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <Suspense>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/mailbox" element={<MailBoxPage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/products/:productId/*" element={<ProductDetailsPage />}/>
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
       </main>
     </div>
   );
